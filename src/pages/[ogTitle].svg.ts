9b0b599c4aff8ac3a5e60023c6482dcf260b37fa
Raman Nikitsenka <rnikitsenka@elinext.com>
2023-08-29 22:47:14 +0200

    fix: fix ogTitle limitation

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -8,11 +8,25 @@ export const get: APIRoute = async ({ params }) => ({
 
 const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
 const posts = Object.values(postImportResult);
+const stripEmojis = (str: string) =>
+  str
+    .replace(
+      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+      ""
+    )
+    .replace(/\s+/g, " ")
+    .replace(/\?/g, "")
+    .trim();
 
 export function getStaticPaths() {
   return posts
     .filter(({ data }) => !data.ogImage)
-    .map(({ data }) => ({
-      params: { ogTitle: data.title },
-    }));
+    .map(({ data }) => {
+      // NOTE: need to remove emoji from ogTitle
+      const ogTitle = stripEmojis(data.title);
+
+      return {
+        params: { ogTitle },
+      };
+    });
 }
