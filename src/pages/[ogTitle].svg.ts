b2b5b1109ee1960ca9e99c27a19364bcc2e24f14
Raman Nikitsenka <rnikitsenka@elinext.com>
2023-08-31 20:44:15 +0200

    fix: try fix ogImage

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -1,22 +1,15 @@
 import { getCollection } from "astro:content";
-import generateOgImage from "@utils/generateOgImage";
+import generateOgImage, { stripEmojis } from "@utils/generateOgImage";
 import type { APIRoute } from "astro";
 
-export const get: APIRoute = async ({ params }) => ({
-  body: await generateOgImage(params.ogTitle),
-});
+export const get: APIRoute = async ({ params }) => {
+  return {
+    body: await generateOgImage(params.ogTitle),
+  };
+};
 
 const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
 const posts = Object.values(postImportResult);
-const stripEmojis = (str: string) =>
-  str
-    .replace(
-      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-      ""
-    )
-    .replace(/\s+/g, " ")
-    .replace(/\?/g, "")
-    .trim();
 
 export function getStaticPaths() {
   return posts
